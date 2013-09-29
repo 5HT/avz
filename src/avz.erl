74572ecfbd2a5bc36b0ecf0b88f5525066c7a76b
Maxim Sokhatsky <maxim@synrc.com>
2013-09-29 16:04:02 +0300

    avz fix for new n2o

--- src/avz.erl
+++ src/avz.erl
@@ -24,6 +24,7 @@ api_event(Name, Args, Term)      -> error_logger:info_msg("Unknown API event: ~p
 
 login_user(User) -> wf:user(User), wf:redirect(?AFTER_LOGIN).
 login(Key, Args)-> case Args of [{error, E}|_Rest] -> error_logger:info_msg("oauth error: ~p", [E]);
-    _ -> case kvs:get(user,Key:email_prop(Args,Key)) of
-              {ok,Existed} -> RegData = Key:registration_data(Args, Key, Existed), (wf_context:page_module()):event({login, RegData});
-              {error,_} -> RegData = Key:registration_data(Args, Key, #user{}), (wf_context:page_module()):event({register, RegData}) end end.
+    _ -> wf:info("AVZ MODULE: ~p",[?CTX#context.module]),
+            case kvs:get(user,Key:email_prop(Args,Key)) of
+              {ok,Existed} -> RegData = Key:registration_data(Args, Key, Existed), (?CTX#context.module):event({login, RegData});
+              {error,_} -> RegData = Key:registration_data(Args, Key, #user{}), (?CTX#context.module):event({register, RegData}) end end.
