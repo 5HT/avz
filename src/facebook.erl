f82c86d4ab31dc62e888b3f9d16c91c9a1be4ed1
Andrey Sergienko <andrey.sergienko@gmail.com>
2015-05-21 17:15:34 +0300

    debug messages commented out

--- src/facebook.erl
+++ src/facebook.erl
@@ -19,8 +19,8 @@ registration_data(Props, facebook, Ori)->
     BirthDay = case proplists:get_value(<<"birthday">>, Props) of
         undefined -> {1, 1, 1970};
         BD -> list_to_tuple([list_to_integer(X) || X <- string:tokens(binary_to_list(BD), "/")]) end,
-    error_logger:info_msg("User Ori: ~p",[Ori]),
-    error_logger:info_msg("Props: ~p",[Props]),
+%    error_logger:info_msg("User Ori: ~p",[Ori]),
+%    error_logger:info_msg("Props: ~p",[Props]),
     Email = email_prop(Props, facebook),
     [UserName|_] = string:tokens(binary_to_list(Email),"@"), Cover = case proplists:get_value(<<"cover">>,Props) of undefined -> ""; P -> case proplists:get_value(<<"source">>,P#struct.lst) of undefined -> ""; C -> binary_to_list(C) end end,
     OldImages = case Ori#user.images of undefined -> []; OldI -> OldI end,
