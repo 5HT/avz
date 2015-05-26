3ee7b277c731cf36e0e8cd495b20e1d1e4ace7d7
Andrey Sergienko <andrey.sergienko@gmail.com>
2015-05-26 11:37:06 +0300

    typo fixed

--- src/userhelper.erl
+++ src/userhelper.erl
@@ -4,7 +4,7 @@
 -include_lib("kvs/include/user.hrl").
 
 updateProplist({K,V},P) ->
-    Prop = case P of undefine -> []; _P -> _P end,
+    Prop = case P of undefined -> []; _P -> _P end,
     case proplists:get_value(K,Prop) of
         undefined -> [{K,V} | Prop];
 	        _ -> lists:keyreplace(K,1,Prop,{K,V})
